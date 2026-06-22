0eba15210db208a02d3118e4b8a21777adf65795
manishmandal0611 <manishmandal0611@gmail.com>
2026-06-22 23:05:00 +0530

    COHORT 3

---
+++ javascript learn/03 Loops and string/script.js
@@ -0,0 +1,92 @@
+// let mark1 = 70
+
+// if (mark1 >= 90){
+//     console.log("Grade A");
+// }
+// else if (mark1 >= 70){
+//     console.log("Grade B");
+// }
+// else if ( mark1 >= 50){
+//     console.log("Grade C");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+
+// let num = 100;
+
+// if (num > 0){
+//     console.log("POSITIVE");
+// }
+// else if (num < 0){
+//     console.log("NEGATIVE");
+// }
+// else{
+//     console.log("Zero");
+// }
+
+
+// let num1 = -20
+
+// if (num1 > 0){
+//     console.log("POSITIVE");
+// }
+// else if (num1 < 0){
+//     console.log("NEGATIVE");
+// }
+// else{
+//     console.log("NEGATIVE");
+// }
+
+// let mark = 65;
+
+// if (mark > 90){
+//     console.log("Grade A");
+// }
+// else if (mark > 80){
+//     console.log("Grade B");
+// }
+// else if (mark > 70){
+//     console.log("Grade C");
+// }
+// else if (mark > 60){
+//     console.log("Grade D");
+// }
+// else{
+//     console.log("fail")
+// }
+
+// /*2. Age Category
+// Age ke hisaab se print karo:
+// 0–12 → Child
+// 13–19 → Teenager
+// 20–59 → Adult
+// 60+ → Senior Citizen */
+
+
+
+
+
+
+
+////// LOOP KA QUESTION HAI YAHA SE
+
+
+// for loop ka estemaal tab karna hota hai jab hame pata ho ki kab rukna hai hame
+// for loop ka condition 
+// for(start; end; change) ye yaad rakho bss kab start karna hai kab end ho ga aur kya change karna hai.
+// for example:-
+for(let i = 1; i<20; i++){
+    console.log(i);
+}
+
+for(let i = 1; i<30; i++){
+    console.log(i);
+}
+
+let fruit = ["apple", "mango", "banana", "pine-apple", "licchi", "kiwi", "namkeen", "khathaaam"];
+
+for (let fruit of fruits){
+    console.log(fruit);
+}
