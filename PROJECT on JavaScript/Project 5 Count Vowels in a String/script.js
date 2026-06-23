83c1b607cd595b9cc8491c395d69b4378bec3c2a
manishmandal0611 <manishmandal0611@gmail.com>
2026-06-23 20:22:38 +0530

    Delete PROJECT on JavaScript directory

--- PROJECT on JavaScript/Project 5 Count Vowels in a String/script.js
+++
@@ -1,9 +0,0 @@
-let str = prompt("Enter a string:").toLowerCase();
-let vowels = "aeiou";
-let count = 0;
-
-for (let char of str) {
-    if (vowels.includes(char)) count++;
-}
-
-console.log(`Number of vowels: ${count}`);
