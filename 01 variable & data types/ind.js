78960fa1ea900d681a67f223407d03060c841f2f
manishmandal0611 <manishmandal0611@gmail.com>
2026-06-27 16:21:52 +0530

    Delete 01 variable & data types directory

--- 01 variable & data types/ind.js
+++
@@ -1,6 +0,0 @@
-// enter the password with the help of dowhile loop.
-do{
-    var pass = prompt("Enter Password");
-} while(pass != '12345');
-
-console.log('you are logged in. welcome to your profile');
