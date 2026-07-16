3a0d7bf2711f79b9811cdb7e52497fbdbde280f7
manishmandal0611 <manishmandal0611@gmail.com>
2026-07-16 15:15:46 +0530

    Added 06 Object and 07 Oops folders

---
+++ javascript learn/06 Objects/script.js
@@ -0,0 +1,542 @@
+// var obj = {
+//     username: "MANISH MANDAL",
+//     Age: 25,
+//     City: "New Delhi"
+// }
+
+// console.log(obj);
+// console.log(obj.Age);
+// console.log(obj.username);
+// console.log(obj.City);
+
+// var user = {
+//     name: 'Virat Kohli',
+//     age: 36,
+//     team: 'india',
+//     isMarried: true,
+//     skills: ['Batting','Dancing','Fielding']
+// }
+
+// console.log(user);
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.team);
+// console.log(user.skills);
+// console.log(user.skills[0]);
+// console.log(user.skills[1]);
+
+// let person = {
+// name: "Aman",
+// age: 25,
+// city: "Bhopal",
+// isStudent: true
+// };
+
+// console.log(person.age);
+
+// let person = { name: "Aman", age: 25 };
+// person.city = "Bhopal";
+// person.age = 78
+
+// console.log(person);
+
+////////////////// this keyword
+//
+
+////////////////////Nested objects
+
+// let user = {
+//     name: "Aman",
+//     address: {
+//         city: "Bhopal",
+//         state: "MP",
+//         pincode: 226022
+//     },
+//     hobbies: ["reading", "coading"]
+// };
+
+// console.log(user.address.city);
+// console.log(user.hobbies[0]);
+
+/////////////Object Destructuring
+
+// let person = { name: "Aman", age: 25, city: "Bhopal" };
+// let { name, age } = person;
+// console.log(name, age);
+
+// let { name: fullName, age: years } = person; ///Rename kiya gya hai
+// console.log(fullName, years);
+
+///// Nested object
+// const user = {
+//     name: "Rohan",
+//     address: {
+//         city: "Lucknow",
+//         state: "UP"
+//     }
+// };
+// const {
+//     address: { city, state }
+// } = user;
+
+// console.log(city, state);
+
+/// Spread with object
+
+// const user = {
+//     name: "Manish",
+//     age: 24
+// };
+// const newUser = {...user };
+// console.log(newUser);
+
+//looping through object
+// for....in loop
+
+// const user = {
+//     name: "Manish",
+//     age: 24,
+//     city: "Lucknow"
+// };
+// for(let key in user){
+//     console.log(key);
+// }
+
+// const user2 = {
+//   name: "Manish",
+//   age: 24,
+//   city: "Lucknow",
+// };
+// for (let key in user2) {
+//   console.log(key, ":", user2[key]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// QUESTION PRACTICE
+// 1. Create an Object
+
+// Create an object for a student with:
+
+// - name
+// - age
+// - course
+
+// Then print all values.
+
+let student = {
+  name: "Manish",
+  age: 24,
+  course: "B.tech",
+};
+
+console.log(student);
+
+
+
+
+
+
+
+
+
+
+
+
+//Given:
+//Print:
+
+// - brand
+// - model
+
+// using both:
+
+// - dot notation
+// - bracket notation
+
+const car = {
+  brand: "BMW",
+  model: "M4",
+  year: 2022,
+};
+
+console.log(car.brand);
+console.log(car.model); // dot se print hua hai
+
+console.log(car["brand"]);
+console.log(car["model"]); // bracket print
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. Update Object Value
+// Change the age of a user from 20 to 25.
+
+const user = {
+  name: "Anubhav",
+  age: 20,
+};
+
+user.age = 25;
+
+console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 4. Add New Property
+
+// Add a new property:
+// isAdmin: true
+
+const user2 = {
+  name: "manoj",
+  age: 56,
+};
+
+user2.isAdmin = "true";
+
+console.log(user2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5. Delete Property
+// Remove the `password` property from the object.
+const account = {
+  username: "john",
+  password: "12345",
+};
+
+delete account.password;
+
+console.log(account);
+
+
+
+
+
+
+
+
+
+
+
+// Count Properties
+// Write a function that returns how many properties an object has.
+// Example: 
+
+countProperties({a:1,b:2,c:3})
+
+function countProperties(obj) {
+    return Object.keys(obj).length;
+}
+
+console.log(countProperties({a:1,b:2,c:3}));
+
+
+
+
+
+
+
+
+
+
+
+// 7. Loop Through Object
+// Print all keys and values from this object.
+// Hint: use
+// for...in
+
+const person = {
+  name: "Rahul",
+  age: 22,
+  city: "Delhi"
+}
+
+for (let key in person) {
+    console.log(key, ":" , person[key]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 8. Check Property Exists
+// Check whether `"email"` exists inside an object or not.
+// Hint:
+// Use: in
+
+
+const user3 = {
+    name: "Manish",
+    age: 24,
+    email: "mainish@gmail.com"
+}
+
+console.log("email" in user3);
+
+
+
+
+
+
+
+
+
+
+
+// Merge Two Objects
+// Merge these two objects into one.
+//Hint:
+//Use:
+//spread operator
+
+
+const obj1 = { a: 1, b: 2 }
+const obj2 = { c: 3, d: 4 }
+
+const result = {
+    ...obj1,
+    ...obj2
+};
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+// 10. Convert Object to Array
+// Convert this object into an array of key-value pairs.
+//Hint:
+// Use:
+// Object.entries()
+
+const user4 = {
+  name: "Aman",
+  age: 21
+}
+
+console.log(Object.entries(user4))
+
+
+
+
+
+
+
+
+
+
+
+// 11. Find Highest Value
+// Find the student with highest marks.
+
+const marks = {
+  Anubhav: 95,
+  Rahul: 82,
+  Aman: 90
+};
+
+let highestStudent = "";
+let highestMarks = 0;
+
+for(let student in marks) {
+  if(marks[student] > highestMarks) {
+    highestMarks = marks[student];
+    higestStudent = student;
+  }
+}
+
+console.log(highestStudent);
+console.log(highestMarks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//12. Sum of Object Values
+// Find total salary.
+//Expected Output: 4500
+
+const salaries = {
+  john: 1000,
+  alex: 2000,
+  bob: 1500
+}
+
+let total = 0;
+
+for (let person in salaries) {
+  total = total + salaries[person];
+}
+
+console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 13. Nested Object Access
+
+// Print:
+
+// - city
+// - pincode
+
+const user5 = {
+  name: "Anubhav",
+  address: {
+    city: "Bhopal",
+    pincode: 462001
+  }
+}
+
+console.log(user5.address.city);
+console.log(user5.address.pincode);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 14. Object Method Practice
+// Create an object with:
+
+// - name
+// - marks
+// - method called `getResult`
+// If marks > 40:
+// "Pass"
+
+// else:
+// "Fail"
+
+const student1 = {
+  name: "Manish",
+  marks: 75,
+
+  getResult() {
+    if (this.marks > 40) {
+      return "Pass";
+    }
+    else {
+      return "fail";
+    }
+  }
+};
+
+console.log(student1.getResult());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let person2 = {
+    firstName: "Rahul",
+    lastName: "Sharma"
+};
+
+let { firstName , lastName } = person2
+
+console.log(firstName,lastName);
---
+++ javascript learn/07 Oops/script.js
@@ -0,0 +1,122 @@
+let hero = {
+    fname: "manish",
+    lname: "kumar",
+    age: 22,
+    getIntro: function () {                /* agar main normal function banata hu toh uska lexical hai
+                                             aur agar main arrow function ko banata hu toh uska kuch lexical function contact nhi hota hai
+                                             */
+        console.log(this.fname);
+    }
+}
+
+hero.getIntro();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (Before ES6) Constructor function
+
+
+//oops  
+function CreateBook(bookName, authorName, pages){  // constructor function bolte hai 
+    this.bookName = bookName
+    this.authorName = authorName
+    this.pages = pages
+    this.getfrontPage = function(){
+        console.log('Book Name', this.bookName);
+        console.log('Author Name', this.authorName);
+        console.log('pages', this.pages);
+    }
+}
+
+let book1 = new CreateBook('Hp - prisoner of Azkaban', 'JK Rowling' ,760)
+let book2 = new CreateBook('rich Dad Poor Dad', 'robert kiyosaki' ,440)
+let book3 = new CreateBook('Sapiens', 'Yuva Noah Harrari1' ,850)
+
+console.log(book1);
+console.log(book2);
+console.log(book3);
+
+book1.getfrontPage();
+book2.getfrontPage();
+book3.getfrontPage();
+
+
+ function MakeStudents(fname, lname, contact, isVerified){ // constructor function
+    this.fname = fname;
+    this.lname = lname;
+    this.contact = contact;
+    this.isVerified = isVerified;
+    this.showProfile = function (){
+        if (this.isVerified){
+            console.log(`Name: ${this.fname} ${this.lname}, Contact: ${this.contact}`);
+        }else{
+            console.log('User Not Verified');
+        }
+
+    }
+ }
+
+ let s1 = new MakeStudents('Abhishek', 'Singh', 1234567890, true)
+ let s2 = new MakeStudents('Ram', 'Mishra', 0987654321, false)
+ let s3 = new MakeStudents('Kunal', 'lal', 123456890, true)
+
+//  console.log(s1);
+//  console.log(s3);
+//  console.log(s2);
+
+s1.showProfile();
+s2.showProfile();
+s3.showProfile();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (After ES6) Class
+
+class MakeStudents1 {
+    constructor(fname, lname, contact, isVerified){
+        this.fname = fname;
+        this.lname = lname;
+        this.contact = contact;
+        this.isVerified = isVerified;
+        this.showProfile = function (){
+            if (this.isVerified){
+                console.log(`Name: ${this.fname} ${this.lname}, Contact: ${this.contact}`);
+            }else{
+                console.log('User Not Verified');
+            }
+    }
+    }
+}
+
+let s5 = new MakeStudents1('Himanshu', 'Kumar', 567890, true)
+let s6 = new MakeStudents1('Ramkala', 'Yadav', 7654321, true)
+let s7 = new MakeStudents1('Surajrawat', 'lal', 89890, false)
+    
+s5.showProfile();
+s6.showProfile();
+s7.showProfile();   
+ 
+ 
+
+
